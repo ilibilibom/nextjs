487a3fc256849b6092f3f6b773a5d26e95b3f2e6
Ilan Diamond <ilan.d@next-insurance.com>
2021-04-25 12:26:14 +0300

    [aws-abtest-poc] - Display version

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import { useRouter } from 'next/router'
 
 function Home({props}) {
   const router = useRouter()
-  const {query:{v,t}} = router;
+  const {query:{v}} = router;
+  const backgroundColor = v === '1' ? "#03c8ff" : "#ffdf59";
   return (
     <div className={styles.container}>
-        <h1>v={v} t={t}</h1>
         <Title 
-          backgroundColor="#03c8ff"
-          version={1}
+          backgroundColor={backgroundColor}
+          version={v}
         />
     </div>
   )
@@ -21,8 +21,6 @@ export async function getServerSideProps(req) {
   // Fetch data from external API
   // const res = await fetch(`https://.../data`)
   // const data = await res.json()
-
-  console.log(req.headers);
 
   // Pass data to the page via props
   return { props: { } }
